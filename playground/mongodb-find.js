c01f91649cbc6e4149f591fc8498a78f7bd91859
sajjadathar <sajjad.athar@gmail.com>
2017-06-22 17:29:31 +0500

    add test GET /todos

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -10,23 +10,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').find({
-  //   _id: new ObjectID('5943d2254c66d907e1164009')
-  // }).toArray()
-  //   .then((docs) => {
-  //     console.log('Todos');
-  //     console.log(JSON.stringify(docs, undefined, 2));
-  //   }, (err) => {
-  //     console.log('Unable to fetch Todos.', err);
-  //   });
-
-  // db.collection('Todos').find().count()
-  //   .then((count) => {
-  //     console.log(`Todos count: ${count}`);
-  //
-  //   }, (err) => {
-  //     console.log('Unable to fetch Todos.', err);
-  //   });
 
   db.collection('Users').find({name: 'soomro'}).toArray()
     .then((docs) => {
